17b4079dacd88a590dca5b639e2242a582b57c5c
Shivvvva <shivva812@gmail.com>
2025-08-27 12:54:04 +0530

    configured tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="w-screen bg-black text-white">
+        App works with tailwind
+      </div>
+    </>
+  );
+};
+
+export default App;
